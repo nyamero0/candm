a38057ae44f2349bf6c4db980a88394536a33ea9
nyamero0 <geca.matic.up@phinmaed.com>
2024-04-19 05:03:01 +0800

    latest

--- src/main/java/com/candmhotel/app/HotelApp/HotelAppApplication.java
+++ src/main/java/com/candmhotel/app/HotelApp/HotelAppApplication.java
@@ -1,23 +1,29 @@
 package com.candmhotel.app.HotelApp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@Configuration
 @Controller
-public class HotelAppApplication {
-
+public class HotelAppApplication implements WebMvcConfigurer{
+  private final static String[] CLASSPATH_RESOURCE_LOCATIONS = {
+    "classpath:/META-INF/resources/",
+    "classpath:/resources/",
+    "classpath:/static/",
+    "classpath:/public/"
+  };
 	public static void main(String[] args) {
 		SpringApplication.run(HotelAppApplication.class, args);
 	}
-
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry){
+    registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+  }
   @GetMapping("/")
   public String IndexPath(){
     return "index";
